cbe7fad57742997d07092b6c2aac2e04ecf014b6
Mohamed Iheb Faiza <fmohamediheb@gmail.com>
2022-02-16 22:09:06 +0100

    💡 Pokemon TCG API Config

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -46,8 +46,9 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js';  // Included with Angular CLI.
-
-
+(window as any).process = {
+    env: { POKEMONTCG_API_KEY: '<INSERT POKEMON TCG API KEY HERE>' },
+  };
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
